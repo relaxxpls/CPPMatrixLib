2163ae1e721427db847b8d84fe38cf8ddfc1b7c5
adityakadoo <aditya1449kadoo@gmail.com>
2021-05-26 17:25:11 +0530

    Tests updated

--- Tests/MatrixTests.cpp
+++ Tests/MatrixTests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <iostream>
 #include "../../matrix.hpp"
 #include "../../matrix_utility.hpp"
 
@@ -35,76 +36,197 @@ TEST(MatirxTests, MatrixInitTests)
 	EXPECT_FALSE(d==a) << "Size constructor / resize() / operator==";
 } 
 
-class BinaryOperatorFixture 
+template<typename T>
+class SampleTest 
 {
 public:
-	int lhs_val;
-	int rhs_val;
-	int add_res_val;
-	int sub_res_val;
-	int mul_res_val;
-	bool eq_res;
-	bool ineq_res;
-	friend std::ostream& operator<<(std::ostream& os, const BinaryOperatorFixture& obj)
+	int type;
+	T A_val=0; size_t A_r=0; size_t A_c=0;
+	T B_val=0; size_t B_r=0; size_t B_c=0;
+	//T C_val=0; size_t C_r=0; size_t C_c=0;
+	T fac=0;
+	bool res=false;
+	SampleTest(int t,T a,size_t ar,size_t ac,
+		T b,size_t br,size_t bc) 
 	{
-		os << "lhs value: " << obj.lhs_val
-		  << "rhs value: " << obj.rhs_val
-		  << "add value: " << obj.add_res_val
-		  << "sub value: " << obj.sub_res_val
-		  << "mul value: " << obj.mul_res_val
-		  << "eq result: " << obj.eq_res
-		  << "ineq result: " << obj.ineq_res;
-		return os;
+		type= t;
+		A_val= a; A_r= ar; A_c= ac;
+		B_val= b; B_r= br; B_c= bc;
 	}
-	BinaryOperatorFixture(int l,int r,int a,int s,int m,bool e,bool i) 
+	SampleTest(int t,T a,size_t ar,size_t ac,T f)
 	{
-		lhs_val= l;
-		rhs_val= r;
-		add_res_val= a;
-		sub_res_val= s;
-		mul_res_val= m;
-		eq_res= e;
-		ineq_res= i;
+		type= t;
+		A_val= a; A_r= ar; A_c= ac;
+		fac= f;
+	}
+	SampleTest(int t,T a,size_t ar,size_t ac,
+		T b,size_t br,size_t bc,
+		bool r) 
+	{
+		type= t;
+		A_val= a; A_r= ar; A_c= ac;
+		B_val= b; B_r= br; B_c= bc;
+		res= r;
 	}
 };
 
-//template <typename T>
-class BinaryOperatorTests : public testing::TestWithParam<BinaryOperatorFixture>
+class IntTests : public testing::TestWithParam<SampleTest<long long int>>
 {
 public:
-	matrix<int> *lhs;
-	matrix<int> *rhs;
-	matrix<int> *add_res;
-	matrix<int> *mul_res;
-	matrix<int> *sub_res;
-	BinaryOperatorTests()
+	matrix<long long int> *A;
+	matrix<long long int> *B;
+	matrix<long long int> *C;
+	IntTests()
 	{
-		lhs= new matrix<int>(3,3,GetParam().lhs_val);
-		rhs= new matrix<int>(3,3,GetParam().rhs_val);
-		add_res= new matrix<int>(3,3,GetParam().add_res_val);
-		sub_res= new matrix<int>(3,3,GetParam().sub_res_val);
-		mul_res= new matrix<int>(3,3,GetParam().mul_res_val);
+		auto in= GetParam();
+		switch(in.type)
+		{
+			case 1: {
+				A= new matrix<long long int>(in.A_r,in.A_c,in.A_val);
+				B= new matrix<long long int>(in.B_r,in.B_c,in.B_val);
+				C= new matrix<long long int>(in.A_r,in.A_c,in.A_val+in.B_val);
+				break;
+			}
+			case 2: {
+				A= new matrix<long long int>(in.A_r,in.A_c,in.A_val);
+				B= new matrix<long long int>(in.B_r,in.B_c,in.B_val);
+				C= new matrix<long long int>(in.A_r,in.A_c,in.A_val-in.B_val);
+				break;
+			}
+			case 3: {
+				A= new matrix<long long int>(in.A_r,in.A_c,in.A_val);
+				B= new matrix<long long int>(in.B_r,in.B_c,in.B_val);
+				C= new matrix<long long int>(in.A_r,in.B_c,in.A_val*in.B_val*in.A_c);
+				break;
+			}
+			case 4: {
+				A= new matrix<long long int>(in.A_r,in.A_c,in.A_val);
+				B= new matrix<long long int>(in.A_r,in.A_c,in.A_val*in.fac);
+				C= new matrix<long long int>();
+				break;
+			}
+			case 5: {
+				A= new matrix<long long int>(in.A_r,in.A_c,in.A_val);
+				B= new matrix<long long int>(in.B_r,in.B_c,in.B_val);
+				C= new matrix<long long int>();
+				break;
+			}
+		}
 	}
-	~BinaryOperatorTests()
+	~IntTests()
 	{
-		delete lhs;
-		delete rhs;
-		delete add_res;
-		delete sub_res;
-		delete mul_res;
+		delete A;
+		delete B;
+		delete C;
 	}
 };
 
-TEST_P(BinaryOperatorTests, BO)
+TEST_P(IntTests, MatirxTests)
 {
-	EXPECT_FALSE((*add_res)==(*lhs)+(*rhs)) << "Addition error at parameter "<<GetParam().lhs_val<<","<<GetParam().rhs_val<<"\n";
-	EXPECT_FALSE((*sub_res)==(*lhs)-(*rhs)) << "Subtraction error\n";
-	EXPECT_FALSE((*mul_res)==(*lhs)*(*rhs)) << "Multiplication error\n";
-	EXPECT_EQ(GetParam().ineq_res,((*lhs)==(*rhs))) << "Equality error\n";
-	EXPECT_EQ(GetParam().eq_res,((*lhs)!=(*rhs))) << "Inequality error\n";
+	auto in= GetParam();
+	switch(in.type)
+	{
+		case 1: {
+			EXPECT_TRUE((*C)==(*A)+(*B)) 
+				<< "Error with operator+ for \nA=\n" << *A << "B=\n" << *B << "C=\n" << *C;
+			*A+= *B;
+			EXPECT_TRUE((*C)==(*A))
+				<< "Error with operator+= for \nA=\n" << *A << "B=\n" << *B << "C=\n" << *C;
+			break;
+		}
+		case 2: {
+			EXPECT_TRUE((*C)==(*A)-(*B)) 
+				<< "Error with operator- for \nA=\n" << *A << "B=\n" << *B << "C=\n" << *C;
+			*A-= *B;
+			EXPECT_TRUE((*C)==(*A))
+				<< "Error with operator-= for \nA=\n" << *A << "B=\n" << *B << "C=\n" << *C;
+			break;
+		}
+		case 3: {
+			EXPECT_TRUE((*C)==(*A)*(*B)) 
+				<< "Error with operator* for \nA=\n" << *A << "B=\n" << *B << "C=\n" << *C;;
+			(*A)*= (*B);
+			EXPECT_TRUE((*C)==(*A))
+				<< "Error with operator*= for \nA=\n" << *A << "B=\n" << *B << "C=\n" << *C;
+			break;
+		}
+		case 4: {
+			EXPECT_TRUE((*B)==(*A)*(in.fac)) 
+				<< "Error with operator* for \nA=\n" << *A << "fac=" << in.fac << "\nB=\n" << *B;
+			EXPECT_TRUE((*A)*(-1)==-(*A)) 
+				<< "Error with operator- for \nA=\n" << *A;
+			EXPECT_TRUE((*B)==(in.fac)*(*A)) 
+				<< "Error with operator* for \nfac=" << in.fac << "\nA=\n" << *A << "\nB=\n" << *B;
+			*A*= in.fac;
+			EXPECT_TRUE((*B)==(*A)) 
+				<< "Error with operator* for \nA=\n" << *A << "fac=" << in.fac << "\nB=\n" << *B;
+			break;
+		}
+		case 5: {
+			EXPECT_EQ(in.res,(*B)==(*A)) 
+				<< "Error with operator== for \nA=\n" << *A << "B=\n" << *B << "res= " << in.res << "\n";
+			EXPECT_EQ(!(in.res),(*B)!=(*A))
+				<< "Error with operator!= for \nA=\n" << *A << "B=\n" << *B << "res= " << in.res << "\n";
+			break;
+		}
+	}
+	
 }
 
-INSTANTIATE_TEST_CASE_P(IntegerTests, BinaryOperatorTests,
+INSTANTIATE_TEST_CASE_P(AdditionTests, IntTests,
 	testing::Values(
-	BinaryOperatorFixture(1,1,2,0,3,true,false)
+/*0*/	SampleTest<long long int>( 1, 0, 3, 3, 0, 3, 3),
+/*1*/	SampleTest<long long int>( 1, 1, 3, 3, -1, 3, 3),
+/*2*/	SampleTest<long long int>( 1, -218'724'171, 3, 3, 313'458'291, 3, 3),
+/*3*/	SampleTest<long long int>( 1, 483'173'198'482'947, 3, 3, -593'206'583'069'789, 3, 3),
+/*4*/	SampleTest<long long int>( 1, 100, 10, 20, -100, 10, 20),
+/*5*/	SampleTest<long long int>( 1, 100, 300, 200, -100, 300, 200),
+/*6*/	SampleTest<long long int>( 1, 100, 500, 750, -100, 500, 750),
+/*7*/	SampleTest<long long int>( 1, -382'594'278'747'059'728, 1000, 3000, 587'208'068'278'698'358, 1000, 3000)
+	));
+
+INSTANTIATE_TEST_CASE_P(SubtractionTests, IntTests,
+	testing::Values(
+/*0*/	SampleTest<long long int>( 2, 0, 3, 3, 0, 3, 3),
+/*1*/	SampleTest<long long int>( 2, 1, 3, 3, -1, 3, 3),
+/*2*/	SampleTest<long long int>( 2, -218'724'171, 3, 3, 313'458'291, 3, 3),
+/*3*/	SampleTest<long long int>( 2, 483'173'198'482'947, 3, 3, -593'206'583'069'789, 3, 3),
+/*4*/	SampleTest<long long int>( 2, 100, 10, 20, -100, 10, 20),
+/*5*/	SampleTest<long long int>( 2, 100, 300, 200, -100, 300, 200),
+/*6*/	SampleTest<long long int>( 2, 100, 500, 750, -100, 500, 750),
+/*7*/	SampleTest<long long int>( 2, -382'594'278'747'059'728, 1000, 3000, 587'208'068'278'698'358, 1000, 3000)
+	));
+
+INSTANTIATE_TEST_CASE_P(MatrixMultiplicationTests, IntTests,
+	testing::Values(
+/*0*/	SampleTest<long long int>( 3, 0, 3, 3, 0, 3, 3),
+/*1*/	SampleTest<long long int>( 3, 1, 3, 3, -1, 3, 3),
+/*2*/	SampleTest<long long int>( 3, -218'724'171, 3, 3, 313'458'291, 3, 3),
+/*3*/	SampleTest<long long int>( 3, 100, 10, 10, -100, 10, 20),
+/*4*/	SampleTest<long long int>( 3, -100, 200, 300, -100, 300, 200),
+/*5*/	SampleTest<long long int>( 3, 100, 700, 500, 100, 500, 750),
+/*6*/	SampleTest<long long int>( 3, -300, 1000, 1000, 500, 1000, 1000)
+	));
+
+INSTANTIATE_TEST_CASE_P(ScalarMultiplicationTests, IntTests,
+	testing::Values(
+/*0*/	SampleTest<long long int>( 4, 0, 3, 3, 10),
+/*1*/	SampleTest<long long int>( 4, 1, 3, 3, -1),
+/*2*/	SampleTest<long long int>( 4, -218'724'171, 3, 3, 313'458'291),
+/*3*/	SampleTest<long long int>( 4, 100, 10, 10, -100),
+/*4*/	SampleTest<long long int>( 4, -100, 200, 300, -100),
+/*5*/	SampleTest<long long int>( 4, 100, 700, 500, 100),
+/*6*/	SampleTest<long long int>( 4, -300, 1000, 1000, 500)
+	));
+
+INSTANTIATE_TEST_CASE_P(EqualityTests, IntTests,
+	testing::Values(
+/*0*/	SampleTest<long long int>( 5, 0, 3, 3, 0, 3, 3, true),
+/*1*/	SampleTest<long long int>( 5, 1, 3, 3, -1, 3, 3, false),
+/*2*/	SampleTest<long long int>( 5, 313'458'291, 3, 3, 313'458'291, 3, 3, true),
+/*3*/	SampleTest<long long int>( 5, 483'173'198'482'947, 3, 3, -593'206'583'069'789, 3, 3, false),
+/*4*/	SampleTest<long long int>( 5, 100, 20, 20, -100, 10, 20, false),
+/*5*/	SampleTest<long long int>( 5, 100, 300, 200, 100, 300, 200, true),
+/*6*/	SampleTest<long long int>( 5, 100, 500, 750, -100, 500, 750, false),
+/*7*/	SampleTest<long long int>( 5, -382'594'278'747'059'728, 1000, 3000, 587'208'068'278'698'358, 1000, 3000, false)
 	));
