e799de4e78b1926b20925036cb56accdffab76cf
“Ashish” <ashishp.nuku@gmail.com>
2021-06-07 22:12:57 +0530

    added factorial, nCr and mod exponentiation

--- modint_utility.hpp
+++ modint_utility.hpp
@@ -4,6 +4,8 @@
 #include "modint.hpp"
 #include <algorithm>
 
+#include <cstring>
+
 template<typename T>
 
 T gcd(T a, T b){
@@ -72,9 +74,88 @@ modint<T> operator/(modint<T> &a, modint<T> &b){
 
     assert(b[0]!=0);
     
-    return modint<T>(a)*=modinv(b);
+    return a/=b;
 }
 
+template <typename T>
+modint<T> powmod(modint<T> a, T b) {
+    if (b == 0) return modint<T>(1, a[1]);
+    else if (b % 2 == 0) {
+        modint<T> x = powmod(a, b / 2);
+        return x * x;
+    }
+    else {
+        modint<T> x = powmod(a, b - 1);
+        return x * a;
+    }
+}
 
+//calculatin n! in mod m
+int largestPower(int n, int p)
+{
+    int x = 0;
+ 
+    // Calculate x = n/p + n/(p^2) + n/(p^3) + ....
+    while (n) {
+        n /= p;
+        x += n;
+    }
+    return x;
+}
 
+modint<int> factmod(int n, int m) {
+    if (n > m) return 0;
+    // Use Sieve of Eratosthenes to find all primes
+    // smaller than n
+    bool isPrime[n + 1];
+    memset(isPrime, 1, sizeof(isPrime));
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = 2 * i; j <= n; j += i)
+                isPrime[j] = 0;
+        }
+    }
+    modint<int> res(1, m);
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            // Find the largest power of prime 'i' that divides n!
+            int k = largestPower(n, i);
+            modint<int> p(i, m);
+            // Multiply result with (i^k) % p
+            res *= powmod(p, k);
+        }
+    }
+    return res;
+}
+
+// Returns n! % p using Wilson's Theorem (assuming p is prime)
+modint<int> modFact(int n, int p) {
+    // n! % p is 0 if n >= p
+    if (p <= n)
+        return 0;
+ 
+    // Initialize result as (p-1)! which is -1 or (p-1)
+    modint<int> res(p - 1, p);
+ 
+    // Multiply modulo inverse of all numbers from (n+1)
+    // to p
+    for (int i = n + 1; i < p; i++){
+        modint<int> k(i, p);
+
+        res *= modinv<int> (k);
+    }
+    return res;
+}
+
+//functuin to calculate nCr in mod m (m must be prime)
+modint<int> combination(int n, int r, int m) {
+    if (n < r) return 0;
+    else if (n == r) return 1;
+    else {
+        modint<int> num = modFact(n, m), denom = modFact(n - r, m) * modFact(r, m); 
+        modint<int> res = num / denom; 
+        return res;
+    }
+
+}
 #endif
