6d023b0e8a04d73cd228a2ea510556020cf31962
SanskarNarlawar <65508315+SanskarNarlawar@users.noreply.github.com>
2021-07-18 00:38:00 +0530

    Add files via upload

---
+++ src/prime_tests.h
@@ -0,0 +1,74 @@
+#pragma once
+#ifndef primes_test
+#define primes_test
+#include <iostream>
+using namespace std;
+using u64 = uint64_t;
+
+u64 binpower(u64 base, u64 e, u64 mod) {
+    u64 result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (u64)result * base % mod;
+        base = (u64)base * base % mod;
+        e >>= 1;
+    }
+    return result;
+}
+
+bool check_composite(u64 n, u64 a, u64 d, int s) {
+    u64 x = binpower(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (u64)x * x % n;
+        if (x == n - 1)
+            return false;
+    }
+    return true;
+};
+
+bool is_prime(long long int n) { // returns true if n is prime, else returns false.
+    if (n < 2)
+        return false;
+
+    int r = 0;
+    u64 d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
+        r++;
+    }
+
+    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+        if (n == a)
+            return true;
+        if (check_composite(n, a, d, r))
+            return false;
+    }
+    return true;
+}
+
+int gcd(int a, int b)
+{
+    // Everything divides 0
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    // base case
+    if (a == b)
+        return a;
+
+    // a is greater
+    if (a > b)
+        return gcd(a - b, b);
+    return gcd(a, b - a);
+}
+
+
+
+
+
+#endif
