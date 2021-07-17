151ced5452a0b7ffc9b0ecfb87929103a2044c37
“Ashish” <ashishp.nuku@gmail.com>
2021-07-17 19:15:24 +0530

    Added tests

---
+++ Build/test/CMakeFiles/Modint_test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ashish/Desktop/CPPMatrixLib/test/ModintTest.cpp" "/home/ashish/Desktop/CPPMatrixLib/Build/test/CMakeFiles/Modint_test.dir/ModintTest.cpp.o"
+  "/home/ashish/Desktop/CPPMatrixLib/test/main.cpp" "/home/ashish/Desktop/CPPMatrixLib/Build/test/CMakeFiles/Modint_test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../lib/googletest/googletest/include"
+  "../lib/googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ashish/Desktop/CPPMatrixLib/Build/src/CMakeFiles/Modint_lib.dir/DependInfo.cmake"
+  "/home/ashish/Desktop/CPPMatrixLib/Build/lib/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,18 @@
-#include "matrix.hpp"
-#include "matrix_utility.hpp"
+#include "modint.hpp"
+#include "modint_utility.hpp"
 
-int main() {
-    // Initialisation of a 3*3 matrix with base value 2
-    matrix<int> B(3, 3, 2);
+using namespace std;
 
-    // Exponentiation
-    matrix<int> M = pow(B, 3);
+int main(){
 
-    // edit single element
-    M(1, 0) = 1;
+    modint<int> p(246,19);
+    modint<int> q(82,19);
+    
+    cout << p / q << "\n" << modinv(q) << endl;
 
-    // M.t() = transpose of M (ie M')
-    matrix<int> X = -2*M.t();
-    // Print matrix
-    std::cout << "matrix X: \n" << X << std::endl;
+    cout << powmod(p , 3) << endl;
 
-    // Span X(1:2, 0:2)
-    // Implementation 1:
-    matrix<int> y1 = X({1, 2}, {0, 2});
-    std::cout << "matrix y1: \n" << y1 << std::endl;
+    cout << factmod(27, 29) << endl;
 
-    // Implementation 2:
-    matrix<int> y2 = X(span(0, 1), span(2, 2));
-    std::cout << "matrix y2: \n" << y2 << std::endl;
-
-    matrix<double> D = {{65, 77, 69, 75, 69},
-                        {61, 74, 70, 66, 68},
-                        {81, 80, 71, 74, 79}};
-
-    // Calculate norm
-    std::cout << "L1 norm of D: " << norm(D, "L1") << std::endl;
-
-    // Print the range of values (max - min) stored in either column (1) or row (2)
-    // std::cout << range(D, 2) << std::endl;
-
-    // Python like range functioning
-    std::cout << "\nrange functions: \n"
-              << range(0, -10, -1)
-              << range(0, 10, 3)
-              << std::endl;
-
-    // Matrix rows
-    matrix<double> c1 = D.column(4);
-    std::cout << "matrix c1: \n" << c1 << std::endl;
-
-    // Matrix columns
-    matrix<double> r1 = D.row(0);
-    std::cout << "matrix r1: \n" << r1 << std::endl;
-
-    // Test QR decomposition:
-    std::tuple<matrix<double>, matrix<double>> qr_tuple= qr(D);
-    matrix<double> q = std::get<0>(qr_tuple), r = std::get<1>(qr_tuple);
-    // Alternate Method:
-    // matrix<int> q(3, 5), r(5, 5);
-    // std::tie(q, r) = qr(D);
-
-    std::cout << "matrix D: \n" << D << std::endl;
-    std::cout << "matrix q*r: \n" << q*r << std::endl;
-    if (q*r == D) {
-        std::cout << "QR Decomposition Works :)" << std::endl;
-    }
-    else {
-        std::cout << "QR Decomposition Failed :(" << std::endl;
-    }
-
-    matrix<double> rM = random_matrix<double> (3,3, {0,20});
-    std::cout << rM;
-    return 0;
+    cout << combination(23, 4, 97) << endl;
 }
